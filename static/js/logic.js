3eb68e4781abbe86969b57d1cf63c52bc7ca6278
Site <site520mmmm@gmail.com>
2024-01-20 17:51:28 -0800

    Your commit message

--- static/js/logic.js
+++ static/js/logic.js
@@ -24,28 +24,25 @@ function markerColor(depth) {
                       '#ccff33';
 }
 
-// Use jQuery to get GeoJSON data
-$.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(geoJsonData) {
-  L.geoJSON(geoJsonData, {
-    // Create a marker for each feature point
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng, {
-        radius: markerSize(feature.properties.mag),
-        fillColor: markerColor(feature.geometry.coordinates[2]),
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-      });
-    },
-    // Add a popup to each marker
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Location: " + feature.properties.place +
-                      "<br>Magnitude: " + feature.properties.mag +
-                      "<br>Depth: " + feature.geometry.coordinates[2] + " km");
-    }
-  }).addTo(myMap);
+// Create the base layers.
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
+
+// Create a baseMaps object.
+var baseMaps = {
+  "Street Map": street,
+  "Topographic Map": topo
+};
+
+// Create an overlay object to hold our overlay.
+var overlayMaps = {
+  // This is initially empty. The earthquakes layer will be added later.
+};
 
 // Create a legend control
 var legend = L.control({ position: 'bottomright' });
@@ -72,3 +69,34 @@ legend.onAdd = function (map) {
 
 // Add the legend to the map
 legend.addTo(myMap);
+
+// Use jQuery to get GeoJSON data
+$.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(geoJsonData) {
+  var earthquakes = L.geoJSON(geoJsonData, {
+    // Create a marker for each feature point
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    },
+    // Add a popup to each marker
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Location: " + feature.properties.place +
+                      "<br>Magnitude: " + feature.properties.mag +
+                      "<br>Depth: " + feature.geometry.coordinates[2] + " km");
+    }
+  });
+
+  // Add the earthquakes layer to the overlayMaps
+  overlayMaps.Earthquakes = earthquakes;
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+});
